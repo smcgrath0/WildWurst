a822b2c84832916895057fdaf2639f6592735e7e
Shane Mcgrath <smcgrath7777@gmail.com>
2019-07-25 15:47:57 -0700

    simple grid outlined

---
+++ firstTown.js
@@ -0,0 +1,80 @@
+class firstTown {
+  constructor(options, pawnOptions){
+    this.options = options;
+    this.pawnOptions = pawnOptions;
+    this.boardArray = [];
+    this.squareArray = [];
+    this.realArray = [];
+    this.gameBoardDom = $("#gameBoard");
+    this.tileDomObject = null;
+    this.row = null;
+    this.col = null;
+  }
+  makeGameBoard() {
+    for (this.row = 0; this.row < this.options.height; this.row++) {
+      this.boardArray[this.row] = [];
+      for (this.col = 0; this.col < this.options.width; this.col++) {
+        var location = {
+          x: this.col, y: this.row
+        }
+        var tile = new Tile(location);
+        this.boardArray[this.row][this.col] = tile;
+        this.tileDomObject = tile.render();
+        this.gameBoardDom.append(this.tileDomObject);
+        // $('.squareContainer').addClass('backOfTile')
+        this.makePawnSquares();
+      }
+    }
+    // for (var i = 5; i < 25; i+=3){
+
+    //   for (var j = 0; j<2; j++){
+    //     $(".square:nth-child(" + (i+j) + ") .squareContainer").addClass("backOfTile");
+    //   }
+    // }
+
+  }
+  makePawnSquares() {
+    for (var row2 = 0; row2 < this.pawnOptions.height; row2++) {
+      this.boardArray[this.row][this.col][row2] = [];
+      for (var col2 = 0; col2 < this.pawnOptions.width; col2++) {
+        var location = {
+          x: col2, y: row2
+        }
+        var square = new Square(location);
+        this.boardArray[this.row][this.col][row2][col2] = square;
+        var squareDomObject = square.render();
+        this.tileDomObject.append(squareDomObject);
+      }
+    }
+  }
+}
+class Tile {
+  constructor(location) {
+    this.location = location;
+    this.domElement = {
+      container: null,
+      contents: null
+    };
+  }
+  render() {
+    this.domElement.container = $("<div>").addClass('square');
+    this.domElement.contents = $("<div>").addClass('squareContainer');
+    this.domElement.container.append(this.domElement.contents);
+    return this.domElement.container;
+  }
+}
+class Square {
+  constructor(location) {
+    this.location = location;
+    this.domElement = {
+      container: null,
+      contents: null
+    };
+  }
+  render() {
+    this.domElement.container = $("<div>").addClass('pawn');
+    this.domElement.contents = $("<div>").addClass('pawnContainer');
+    this.domElement.container.append(this.domElement.contents);
+    return this.domElement.container;
+  }
+}
--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function killJohn(){
+$(document).ready(initializeApp);
 
+// var tileConfig = {
+//   height: 3,
+//   width: 3,
+// }
+// var squareConfig = {
+//   height: 4,
+//   width: 4
+// }
+function initializeApp(){
+  var game = new firstTown({height: 5,width:5},{height:5,width:5});
+  console.log(game);
+  game.makeGameBoard();
 }
