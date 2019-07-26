b91d3fa52645c1e2e2d7f138012f81b2410bead6
Shane Mcgrath <smcgrath7777@gmail.com>
2019-07-26 15:46:29 -0700

    screen tracks player

--- jscomponents/home.js
+++ jscomponents/home.js
@@ -1,5 +1,11 @@
-class Home {
-  constructor(){
-
+class HomeDisplay {
+  constructor(boardArray, currentPlayerPosition){
+    this.boardArray = boardArray;
+    this.currentPlayerPosition = currentPlayerPosition;
+    console.log(this.boardArray);
+  }
+  createHomeDisplay(){
+    this.displayDiv = $("<div>").addClass("home");
+    $("#buildingContainer").append(this.displayDiv)
   }
 }
